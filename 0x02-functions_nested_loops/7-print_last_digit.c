1095e90ede8f7d8c3c823c010952c217505944cd
Zakaria0o <zakaria.hhn@gmail.com>
2023-06-20 17:49:01 -0700

    hi

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,18 +7,18 @@
  */
 int print_last_digit(int n)
 {
-	int a;	
+	int a;
 
-	a = n % 10;	
+	a = n % 10;
 	if (a < 0)
 	{
 		_putchar(-a + 48);
 		return (-a);
 	}
 	else
-	{	
+	{
 		_putchar(a + 48);
 		return (a);
 	}
-	
+
 }
