2e685387a2825396e0fd56b29166b42a38ee2473
Zakaria0o <Zakaria0o@gmail.com>
2023-10-15 17:04:57 -0700

    struc typdef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-	#include <stdlib.h>
-	#include "dog.h"
-
-
-	/**
-	 * print_dog - prints a struct dog
-	 * @d: struct dog to print
-	 */
-	void print_dog(struct dog *d)
-	{
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
+ */
+void print_dog(struct dog *d)
+{
 		if (d == NULL)
 			return;
 
@@ -20,4 +18,4 @@
 
 
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
-	}
+}
